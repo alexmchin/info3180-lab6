e27858055cdba31ecb03b246dcd21d02b0994543
alexmchin <alexmchin12@gmail.com>
2022-04-04 20:31:28 -0500

    3rd Commit

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -3,6 +3,18 @@
         <li v-for="article in articles" class="news__item"> <img class="images" :src = "article.urlToImage"/> {{ article.title }}
         {{article.description}}</li>
 
+        <form  @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+            <div class="input-group mx-sm-3 mb-2">
+                <label class="visually-hidden" for="search">Search</label>
+                <input type="search" name="search" v-model="searchTerm"
+        id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+        search term here" />
+                <button class="btn btn-primary mb-2">Search</button>
+            </div>
+            <p>You are searching for {{ searchTerm }}</p>
+        </form>
+
+
     </ul>
 </template>
 
@@ -10,17 +22,18 @@
 export default {
  data() {
  return {
- articles: []
+ articles: [],
+ searchTerm: ''
  }
+ 
  },
- created() {
-let self = this;
-
- fetch('https://newsapi.org/v2/top-headlines?country=us',
-{
+ methods: {
+ searchNews() {
+ let self = this;
+ fetch('https://newsapi.org/v2/everything?q='+
+self.searchTerm + '&language=en', {
  headers: {
-    'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
-
+ 'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
  }
 })
  .then(function(response) {
@@ -30,6 +43,7 @@ let self = this;
  console.log(data);
  self.articles = data.articles;
  });
+ }
  }
 };
 </script>
