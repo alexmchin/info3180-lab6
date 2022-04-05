a9905ad9e4ad77f3d32cd47414fe1db56eb9c5c3
alexmchin <alexmchin12@gmail.com>
2022-04-04 20:14:48 -0500

    2nd Commit

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,16 +1,54 @@
 <template>
- <ul class="news__list">
- <li class="news__item">News item 1</li>
- <li class="news__item">News item 2</li>
- <li class="news__item">News item 3</li>
- </ul>
+    <ul class="news__list">
+        <li v-for="article in articles" class="news__item"> <img class="images" :src = "article.urlToImage"/> {{ article.title }}
+        {{article.description}}</li>
+
+    </ul>
 </template>
 
 <script>
 export default {
  data() {
- return {};
+ return {
+ articles: []
+ }
  },
+ created() {
+let self = this;
+
+ fetch('https://newsapi.org/v2/top-headlines?country=us',
+{
+ headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+
+ }
+})
+ .then(function(response) {
+ return response.json();
+ })
+ .then(function(data) {
+ console.log(data);
+ self.articles = data.articles;
+ });
+ }
 };
 </script>
+
+<style>
+  .news__item{
+  display: gridbox;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-gap: 20px;
+  margin: 15px;
+
+  }
+ .images{
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+    width:100%;
+     height: 20vw;
+     object-fit: cover;
+ }
+</style>
 
